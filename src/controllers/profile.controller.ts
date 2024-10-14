6f043c4bf090460404f48f8d162a5e3b4476ad52
Matheus Dos Santos <matheus2050@gmail.com>
2024-10-14 16:27:20 -0300

    criado profile controller

---
+++ src/controllers/profile.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from 'express';
+import { ProfileService } from '../services/profile.service';
+
+export class ProfileController {
+    private profileService: ProfileService;
+
+    constructor() {
+        this.profileService = new ProfileService();
+    }
+
+    // Atualiza dados do usuario
+    async updateUser(req: Request, res: Response) {
+        try {
+            const { name, email, password } = req.body;
+            const userId = req.userLogged.userId;
+            const user = await this.profileService.updateUser(name, email, password, userId);
+            const bodyResp = { id: user.id, name: user.name, email: user.email}
+            return res.status(200).json(bodyResp);
+        } catch (error) {
+            return res.status(500).json({ message: 'Erro ao atualizar o usuário.', error});
+        }
+    }
+
+    // Obtem dados do user
+    async getUser(req: Request, res: Response) {
+        try {
+            const userId = req.userLogged.userId;
+            const user = await this.profileService.getUser(userId);
+            return res.status(200).json(user);
+        } catch (error) {
+            return res.status(500).json({ message: 'Erro ao atualizar o usuário.', error});
+        }
+    }
+}
